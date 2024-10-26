9b3660f3aa924b3389717112b7352b97680ae007
ufpnjh17 <124604760+ufpnjh17@users.noreply.github.com>
2024-10-26 19:59:14 +0300

    Лабораторная 2

---
+++ Лабораторная 2/lab2_1.py
@@ -0,0 +1,17 @@
+salary = 5000
+spend = 6000
+months = 10
+increase = 0.03
+
+required_capital = 0
+current_spend = spend
+
+for month in range(months):
+    deficit = current_spend - salary
+    if deficit > 0:
+        required_capital += deficit
+    current_spend *= (1 + increase)
+
+required_capital = round(required_capital)
+
+print(f"Подушка безопасности, чтобы протянуть {months} месяцев без долгов:", required_capital)
